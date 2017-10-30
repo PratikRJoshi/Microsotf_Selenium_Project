db15cdb4ae5c74df2b3570efe18ae7593fef7b1e
Pratik Joshi <pratik.joshi@appdynamics.com>
2017-10-29 19:57:18 -0700

    Got the logic working, but without file read operations Need to add the logout and log back in operation as a new user

--- src/main/java/microsoft/App.java
+++ src/main/java/microsoft/App.java
@@ -1,11 +1,10 @@
 package microsoft;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,19 +13,15 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws InterruptedException, FileNotFoundException {
-
-        String searchTextFile = "/Users/pratik.joshi/Documents/Projects/Microsoft_Rewards_Selenium/resources/SearchTexts";
 
         String webPageURL = "https://login.live.com/login.srf";
         String bingURL = "https://www.bing.com/";
@@ -35,13 +30,10 @@ public class App {
 
         String userNameTextBox = "//*[@id=\"i0281\"]/div[1]/div/div[1]/div[2]/div/div/div[2]/div[2]/div/div[2]/div";
         String nextButton = "//*[@id='idSIButton9']";
-        String signInButton = "//*[@id='i0118']";
 
-        // Create file reader to read the search texts from a file
-        FileReader fileReader = null;
-        BufferedReader bufferedReader = null;
+        final int COUNTER_LIMIT = 30;
 
-        // Create a new instance of Firefox driver
+        // Create a new instance of Chrome driver
         WebDriver chromeDriver = new ChromeDriver();
         Actions actions = new Actions(chromeDriver);
 
@@ -55,7 +47,7 @@ public class App {
         actions.build().perform();
 
         chromeDriver.findElement(By.xpath(nextButton)).click();
-        Thread.sleep(2000);
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
 
         WebElement passwordElement = chromeDriver.findElement(By.className("form-control"));
         actions.moveToElement(passwordElement);
@@ -64,19 +56,24 @@ public class App {
         actions.build().perform();
 
         chromeDriver.findElement(By.xpath(nextButton)).click();
-        Thread.sleep(2000);
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
 
         // Go to Bing homepage
         chromeDriver.get(bingURL);
 
-        final WebElement searchButton = chromeDriver.findElement(By.name("q"));
-//        searchButton.sendKeys("Rose\n");
+        WebElement searchBox;
 
-        try (Stream<String> lineStream = Files.lines(Paths.get(searchTextFile))) {
-            lineStream.forEach(line -> searchButton.sendKeys(line+"\n"));
-            Thread.sleep(TimeUnit.SECONDS.toMillis(2));
-        } catch (IOException e) {
-            e.printStackTrace();
+        int counter = 0;
+        Random random = new Random();
+        try {
+            while (++counter <= COUNTER_LIMIT) {
+                searchBox = chromeDriver.findElement(By.name("q"));
+//                searchBox.sendKeys(UUID.randomUUID().toString() + "\n");
+                searchBox.sendKeys((char)(random.nextInt('z' - 'a') + 'a') + "\n");
+            }
+        } finally {
+            chromeDriver.close();
+
         }
     }
 }
